8738963f8a9410a2c95ea212f3b21bd7897ede24
Enes <enes.barut@gmx.com>
2024-11-03 16:43:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './**/templates/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
